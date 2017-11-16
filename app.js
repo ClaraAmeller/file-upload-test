52feaba91914d4f9b20a2fc59c7beaf192e9ef1c
ClaraAmeller <claraameller7@gmail.com>
2017-11-16 11:06:17 +0100

    Files configuration

--- app.js
+++ app.js
@@ -2,10 +2,10 @@
 
 const express = require('express');
 const path = require('path');
-const favicon = require('serve-favicon');
 const logger = require('morgan');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const index = require('./routes/index');
 
@@ -25,7 +25,17 @@ app.use(bodyParser.urlencoded({
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+
+// Routes
 app.use('/', index);
+
+// Mongoose connection
+mongoose.Promise = Promise;
+mongoose.connect('mongodb://localhost/lab-express-file-upload', {
+    keepAlive: true,
+    reconnectTries: Number.MAX_VALUE,
+    useMongoClient: true
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,36 @@
-var express = require('express');
-var router = express.Router();
+'use strict';
+
+const express = require('express');
+const router = express.Router();
+const multer = require('multer');
+const Picture = require('../models/pictures');
+const upload = multer({
+    dest: './public/uploads/'
+});
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', (req, res) => {
+    res.render('index');
 });
+
+// Route to upload from project base path
+
+/* POST File with Multer */
+router.post('/upload', upload.single('photo'), (req, res, next) => {
+    const pic = new Picture({
+        name: req.body.name,
+        pic_path: `/uploads/${req.file.filename}`,
+        pic_name: req.file.originalname
+    });
+
+    pic.save((err) => {
+        if (err) {
+            next(err);
+        } else {
+            res.redirect('/');
+        }
+    });
+});
+
 
 module.exports = router;
